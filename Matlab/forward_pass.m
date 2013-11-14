14772fff9d404183919979754224b16169707301
Ibrahim <imusba@umich.edu>
2013-11-14 14:51:01 -0500

    Vectorized Forwardpass
    Need to vectorize backpropagation

--- Matlab/forward_pass.m
+++ Matlab/forward_pass.m
@@ -6,9 +6,10 @@ n_l = NN{3};
 n_nodes = NN{4};
 
 a{1} = ip;
+m = size(ip,2);
 
 for i = 1:n_l-1
-    z{i+1} = W{i}*a{i} + b{i};
+    z{i+1} = W{i}*a{i} + repmat(b{i},1,m);
     a{i+1} = 1./(1+exp(-z{i+1}));
 end
 
