4b1a2782c0511a8e235c5a64eddf7bca875f879d
Apexit <apexit@umich.edu>
2013-12-04 23:32:35 -0500

    ML: NN and rbm combined

--- Matlab_ML/RBM/run_visible.m
+++ Matlab_ML/RBM/run_visible.m
@@ -1,4 +1,4 @@
-function [h label]= run_visible( data,weights, numVisible, numHidden)
+function [h label]= run_visible( data,weights,b, numVisible, numHidden)
 %     
 %     Assuming the RBM has been trained (so that weights for the network have been learned),
 %     run the network on a set of visible units, to get a sample of the hidden units.
@@ -23,14 +23,15 @@ function [h label]= run_visible( data,weights, numVisible, numHidden)
     data = [ones(num_examples,1) data];
 
 %     Calculate the activations of the hidden units.
+    weights = [b' ; weights'] ; % append b as the first row
     hidden_activations = data*weights;
 %     Calculate the probabilities of turning the hidden units on.
     hidden_probs = logistic(hidden_activations);
 %     Turn the hidden units on with their specified probabilities.
     hidden_states = hidden_probs > 0.5;
 %     Always fix the bias unit to 1.
-     hidden_states(:,1) = 1;
+%      hidden_states(:,1) = 1; % comment it for now
   
 %     Ignore the bias units and return state or probability.
-     h = hidden_probs(:,2:end);
+     h = hidden_probs;%(:,2:end);
     [m,label] = max(h,[],2);
